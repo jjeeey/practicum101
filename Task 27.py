138a6f80cadfa6fd330e2af02a2acbdb046ca22a
jjeeey <73061094+jjeeey@users.noreply.github.com>
2020-12-11 10:53:15 +0500

    Add files via upload

---
+++ Task 27.py
@@ -0,0 +1,25 @@
+"""
+Имя проекта: practicum_1
+Номер версии: 1.0
+Имя файла: Task 27.py
+Автор: 2020 © Ю.А. Мазкова, Челябинск
+Лицензия использования:  CC BY-NC 4.0 (https://creativecommons.org/licenses/by-nc/4.0/deed.ru)
+Дата создания: 11/12/2020
+Дата последней модификации: 11/12/2020
+Связанные файлы/пакеты: numpy, random
+Описание: Решение задачи № 1-101 практикума № 1
+Дано вещественное число A. Вычислить f(A), если f(x) = 0 при x ≤ 0; f(x) = x2 − x при 0 < x < 1, в противном случае f(x) = x2 − sin(πx2).
+#версия Python: 3.9.0
+"""
+import math
+A = float(input("Введите число А:"))
+x = A
+if x <= 0:
+    fx = 0
+    print("x <= 0; f(a) =:", fx)
+elif 0 < x < 1:
+    fx = math.pow(x, 2) - x
+    print("0 < x < 1; f(a) =:", fx)
+else:
+    fx = math.pow(x, 2) - math.sin((math.pi * math.pow(x, 2)))
+    print("x >= 1; f(a) =:", fx)
